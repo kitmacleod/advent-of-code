a600534268375cd7a37f8fc859ff572f8bd82dce
Kit Macleod <kitmacleod@gmail.com>
2023-12-08 11:17:44 +0000

    Python set up

---
+++ python/test_solution.py
@@ -0,0 +1,10 @@
+import pytest
+from solution import solve_part1, solve_part2
+
+
+def test_solve_part1():
+    assert solve_part1("") == "expected result"
+
+
+def test_solve_part2():
+    assert solve_part2("") == "expected result"
